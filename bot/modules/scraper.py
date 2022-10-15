9c41e0e8dcc828be8e7bdf889db00c9574336600
Sourcery AI <>
2022-10-15 08:18:03 +0000

    'Refactored by Sourcery'

--- bot/modules/scraper.py
+++ bot/modules/scraper.py
@@ -11,34 +11,12 @@ from bot.helper.telegram_helper.message_utils import sendMessage
 
 def scrape(update, context):
     user_id_ = update.message.from_user.id
-    if PAID_SERVICE is True:
-        if user_id_ in PAID_USERS and OWNER_ID:
-            message:Message = update.effective_message
-            link = None
-            if message.reply_to_message: link = message.reply_to_message.text
-            else:
-                link = message.text.split(' ', 1)
-                if len(link) != 2:
-                    help_msg = "<b>Send link after command:</b>"
-                    help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {link}" + "</code>"
-                    help_msg += "\n<b>By replying to message (including link):</b>"
-                    help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {message}" + "</code>"
-                    return sendMessage(help_msg, context.bot, update.message)
-                link = link[1]
-            try: link = re.match(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", link)[0]
-            except TypeError: return sendMessage('Not a valid link.', context.bot, update)
-            links = []
-            res = requests.get(link)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            x = soup.select('a[href^="magnet:?xt=urn:btih:"]')
-            for a in x:
-                links.append(a['href'])
-            for o in links:
-#                 print(o)
-                sent = sendMessage(f"{o}", context.bot, update.message)
-        else:
-            sendMessage(f"Buy Paid Service to use Feature.", context.bot, update.message)
-    else:
+    if (
+        PAID_SERVICE is True
+        and user_id_ in PAID_USERS
+        and OWNER_ID
+        or PAID_SERVICE is not True
+    ):
         message:Message = update.effective_message
         link = None
         if message.reply_to_message: link = message.reply_to_message.text
@@ -53,15 +31,15 @@ def scrape(update, context):
             link = link[1]
         try: link = re.match(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", link)[0]
         except TypeError: return sendMessage('Not a valid link.', context.bot, update)
-        links = []
         res = requests.get(link)
         soup = BeautifulSoup(res.text, 'html.parser')
         x = soup.select('a[href^="magnet:?xt=urn:btih:"]')
-        for a in x:
-            links.append(a['href'])
+        links = [a['href'] for a in x]
         for o in links:
-#                print(o)
+#                 print(o)
             sent = sendMessage(f"{o}", context.bot, update.message)
+    else:
+        sendMessage("Buy Paid Service to use Feature.", context.bot, update.message)
 
 
 
